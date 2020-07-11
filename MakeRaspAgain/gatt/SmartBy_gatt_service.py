09b383c511cf33281d2016214107b7725f99e21a
ParkMH <46748131+M16Lukas@users.noreply.github.com>
2020-07-12 00:34:05 +0900

    Add files via upload

--- MakeRaspAgain/gatt/SmartBy_gatt_service.py
+++ MakeRaspAgain/gatt/SmartBy_gatt_service.py
@@ -1,7 +1,14 @@
+import sys
+import os
+import threading
 import dbus
 import dbus.mainloop.glib
+
 from time import sleep, time
+
+import RPi.GPIO as GPIO
 import numpy as np
+import subprocess as sp
  
 try:
     from gi.repository import GObject
@@ -12,13 +19,57 @@ from bluez_components import *
  
 mainloop = None
 
+# Setting up GPIO
+GPIO_LOCK = 18
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_LOCK, GPIO.OUT)     # using GPIO 18
+
+"""
+Linear Actuator(L12-30-100-6-R)
+- Stroke Length       = 30 mm
+- Gear reduction Rate = 100
+- Voltage             = 6 Volt (DC)
+- Controller          = R (RC Servo Integrated)
+"""
+lock_pwm = GPIO.PWM(GPIO_LOCK, 500)   # Frequency = 0.5 kHz (2 ms)
+lock_pwm.start(100)                   # Duty Cycle = 100
+class LockOnOff(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    
+    # 1.0 ms pulse commands the controller to fully retract the actuator 
+    def On(self):
+        for dc in range(95,101,1):
+            lock_pwm.ChangeDutyCycle(dc)
+            sleep(0.1)
+            if dc == 100:
+                self.Stop()
+
+    # 2.0 ms pulse signals it to fully extend
+    def Off(self):
+        for dc in range(48,40,-1):
+            print(dc)
+            lock_pwm.ChangeDutyCycle(dc)
+            sleep(0.1)
+            if dc == 41:
+                self.Stop()
+
+    def Stop(self):
+        for dc in range(0,20,1):
+            lock_pwm.ChangeDutyCycle(dc)
+            sleep(0.5)
+
+
 def check_password(value):
     Crypto_Code = [0x01, 0x0f, 0x05, 0x0d, 0x08, 0x0b]
     ch = np.equal(Crypto_Code, value)
     if False in ch:
         print("get out!!!!!")
+        LockOnOff().On()
     else:
         print("come on buddy")
+        LockOnOff().Off()
 
 class cmdChrc(Characteristic):
     CMD_UUID = '0d0f0fe1-0e65-1d70-855e-02505f9c40e1'
@@ -27,13 +78,9 @@ class cmdChrc(Characteristic):
         Characteristic.__init__(
             self, bus, index,
             self.CMD_UUID, 
-            ['read', 'write'],
+            ['write'],
             service)
         self.value = [ 0x00 for i in range(1024) ]
- 
-    def ReadValue(self, options):
-        print('RowCharacteristic Read: ' + repr(self.value))
-        return self.value
  
     def WriteValue(self, value, options):
         print('RowCharacteristic Write: ' + repr(value))
@@ -124,6 +171,13 @@ def main():
         mainloop.run()
     except KeyboardInterrupt:
         print("Finished")
+        lock_pwm.stop()
+        GPIO.cleanup()
+        os._exit(1)
+    
+    lock_pwm.stop()
+    GPIO.cleanup()
+    os._exit(1)
  
  
 if __name__ == '__main__':
